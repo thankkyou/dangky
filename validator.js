01cfd7df20900bb2ed0916b3bc9be29e1b94cb1f
thankkyou <138964779+thankkyou@users.noreply.github.com>
2024-10-28 09:27:16 +0700

    mo phat

--- validator.js
+++ validator.js
@@ -1,13 +1,35 @@
 function Validator (options) {
     var formElement = document.querySelector(options.form);
 
-    
+    if (formElement) {
+
+        options.rules.forEach(function(rule){
+            
+            var inputElement = formElement.querySelector(rule.selector);
+
+            if (inputElement) {
+                inputElement.onblur = function
+            }
+
+        }) ;
+    }
+
 }
 
 Validator.isRequired = function() {
+     return {
+        selector: selector, 
+        test: function() {
 
+        }
+     };
 }
 
 Validator.isEmail = function() {
-    
+    return {
+        selector: selector, 
+        test: function() {
+
+        }
+     };
 }
